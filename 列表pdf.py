bfdfc7ab40d2c3ad29ac3b6af0c9d2350d3fa4df
13203933832 <49905194+13203933832@users.noreply.github.com>
2019-07-20 16:56:14 +0800

    Python

---
+++ 列表pdf.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+namesList = ['xiaoWang','xiaoZhang','xiaoHua']
+for name in namesList:
+    print(name)
+'''
+
+'''
+namesList = ['xiaoWang','xiaoZhang','xiaoHua']
+length = len(namesList)
+i = 0
+while i<length:
+    print(namesList[i])
+    i+=1
+'''
